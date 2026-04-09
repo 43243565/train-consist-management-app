93c36e6c21c5fc46867cc4b7cc6c9bf89711114d
Shivampuri-07 <shivampuri901@gmail.com>
2026-04-09 09:39:42 +0530

    UC17 sort bogie

--- src/TrainConsistMgmt.java
+++ src/TrainConsistMgmt.java
@@ -1,49 +1,24 @@
 /**
  * MAIN CLASS: TrainConsistMgmt
- * Use Case 15: Safe Cargo Assignment
- * author shivam pur
+ * Use Case 17: Sort Bogie Names using Arrays.sort()
+ *author shivam
  */
 
-// ----- CUSTOM RUNTIME EXCEPTION -----
-class CargoSafetyException extends RuntimeException {
-    public CargoSafetyException(String msg) {
-        super(msg);
-    }
-}
-
-// ----- GOODS BOGIE CLASS -----
-class GoodsBogie {
-    String shape;
-    String cargo;
-
-    public GoodsBogie(String shape) {
-        this.shape = shape;
-    }
-
-    public void assignCargo(String cargo) {
-        try {
-            if (shape.equals("Rectangular") && cargo.equals("Petroleum")) {
-                throw new CargoSafetyException("Unsafe Cargo ❌");
-            }
-            this.cargo = cargo;
-            System.out.println("Cargo Assigned ✔️: " + cargo);
-
-        } catch (CargoSafetyException e) {
-            System.out.println("Exception: " + e.getMessage());
-        } finally {
-            System.out.println("Operation Completed 🔄");
-        }
-    }
-}
+import java.util.Arrays;
 
 // ----- MAIN CLASS -----
 public class TrainConsistMgmt {
+
     public static void main(String[] args) {
 
-        GoodsBogie b1 = new GoodsBogie("Cylindrical");
-        b1.assignCargo("Petroleum");
+        String[] bogies = {
+                "Sleeper", "AC Chair", "First Class", "General", "Luxury"
+        };
 
-        GoodsBogie b2 = new GoodsBogie("Rectangular");
-        b2.assignCargo("Petroleum");
+        Arrays.sort(bogies);
+
+        for (String b : bogies) {
+            System.out.println(b);
+        }
     }
 }
